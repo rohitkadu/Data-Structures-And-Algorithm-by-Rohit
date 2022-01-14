c5738f61d36460043606c74c72f5f9c4c5e729dd
rohitkadu <rohitkadu3102@gmail.com>
2022-01-14 23:00:57 +0530

    Bubble Sort Code

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,33 +1,32 @@
-// Seletion Sort
+// Bubble Sort 
 /*
-Selection Sort - select the index ( i.e. ' i ' value )
-compare it for j = i + 1 till j = n-1 and swap accordingly .
-
-Left Side : sorted 
-Right Side : Unsorted
+Bubble Sort - Compare the adjacent elements and pop / bubble out largest number at right side .
+Do this n-1 times and comparisons i = 0 to i = n - counter .
 */
 #include<iostream>
 using namespace std;
 
 
-void selectionSort(int arr[] , int n )
+void bubbleSort(int arr[] , int n)
 {
-    int i , j ;
+    int i ;
+    int counter = 1 ;
 
-    for( i=0 ; i<n ; i++ )
+    while( counter < n )
     {
-        for( j=i+1 ; j<n ; j++ )
+        for( i=0 ; i<n-counter ; i++ )
         {
-            if( arr[j] < arr[i] )
+            if( arr[i] > arr[i+1] )
             {
                 int temp = arr[i] ;
-                arr[i] = arr[j] ;
-                arr[j] = temp ;
+                arr[i] = arr[i+1] ;
+                arr[i+1] = temp ;
             }
         }
+        counter++ ;
     }
 
-    for( i=0 ; i<n ; i++)
+    for( i=0 ; i<n ; i++ )
     {
         printf("%d ",arr[i]);
     }
@@ -57,8 +56,8 @@ int main()
         printf("%d ",arr[i]);
     }
 
-    cout<<"\n\nSorted Array ( Selection Sort ) : " ;
-    selectionSort(arr,n);
+    cout<<"\n\nSorted Array ( Bubble Sort ) : " ;
+    bubbleSort(arr,n);
     cout<<endl<<endl;
     return 0;
 }
