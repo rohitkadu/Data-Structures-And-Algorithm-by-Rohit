bdbd3395aefa57f85fcd1c0b6d90e8b51c065899
rohitkadu <rohitkadu3102@gmail.com>
2022-01-16 00:07:55 +0530

    Selection Sort

---
+++ SortingTechniques/selectionSort.cpp
@@ -0,0 +1,64 @@
+// Seletion Sort
+/*
+Selection Sort - select the index ( i.e. ' i ' value )
+compare it for j = i + 1 till j = n-1 and swap accordingly .
+
+Left Side : sorted 
+Right Side : Unsorted
+*/
+#include<iostream>
+using namespace std;
+
+
+void selectionSort(int arr[] , int n )
+{
+    int i , j ;
+
+    for( i=0 ; i<n ; i++ )
+    {
+        for( j=i+1 ; j<n ; j++ )
+        {
+            if( arr[j] < arr[i] )
+            {
+                int temp = arr[i] ;
+                arr[i] = arr[j] ;
+                arr[j] = temp ;
+            }
+        }
+    }
+
+    for( i=0 ; i<n ; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+}
+
+
+int main()
+{
+    int n ;
+    cout<<"\nEnter Size of Array : ";
+    cin>>n;
+
+    int arr[n];
+
+    int i ;
+    cout<<"\nEnter Elements of array :\n";
+    for( i=0 ; i<n ; i++)
+    {
+        printf("\tElement %d = ",i+1);
+        cin>>arr[i];
+    }
+
+    cout<<"\nOriginal Array : ";
+    for( i=0 ; i<n ; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+    cout<<"\n\nSorted Array ( Selection Sort ) : " ;
+    selectionSort(arr,n);
+    cout<<endl<<endl;
+    return 0;
+}
