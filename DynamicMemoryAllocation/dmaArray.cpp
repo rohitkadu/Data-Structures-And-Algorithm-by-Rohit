05ee96a1c75ba6a8ba11304b713ca99473605c07
rohitkadu <rohitkadu3102@gmail.com>
2022-01-16 22:27:06 +0530

    Dynamic Array

---
+++ DynamicMemoryAllocation/dmaArray.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n ;
+    cout<<"\n\n***Dynamic Array***\n\n";
+    cout<<"Enter Size of Array : ";
+    cin>>n;
+
+    int *arr = new(nothrow) int[n];
+    if(!arr) {
+        cout<<"\t!!!Memory Allocation Failed" ;
+    } else {
+        cout<<"\n*Memory Allocated Successfully .";
+        printf("Memory Block of \'%d\' created .\n\n",n*sizeof(int));
+    }
+
+    cout<<"Enter Array Elements : ";
+    for( int i=0 ; i<n ; i++ )
+    {
+        cin>>arr[i];
+    }
+
+    cout<<"\n\nArray : ";
+    for( int i=0 ; i<n ; i++ )
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    delete[] arr ;
+    cout<<"\n\t- Memory Deallocated .\n\n";
+    return 0;
+}
