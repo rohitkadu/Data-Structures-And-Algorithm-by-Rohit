40a235b52db9022f6e433debc2467a8beca69d51
Rohit Kadu <rohitkadu3102@gmail.com>
2023-06-14 16:03:58 +0530

    Pattern 14

---
+++ Patterns/pattern_14.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void imp(){
+
+  #ifndef ONLINE_JUDGE
+      freopen("../input.txt","r",stdin);
+      freopen("../output.txt","w",stdout);
+  #endif
+}
+
+void pattern(int input) {
+    for( int i=1 ; i<=input ; i++ )
+    {
+        for( char ch='A' ; ch<'A'+i ; ch++ )
+        {
+            cout<<ch<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+int main()
+{
+    imp();
+
+    int n;
+    cin>>n;
+    int input;
+
+
+    for(int i=1 ; i<=n ; i++)
+    {
+        cin>>input;
+        pattern(input);
+    }
+
+
+    return 0;
+}
