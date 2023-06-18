39448503d369d97d4e198b6d1165687f5222517c
Rohit Kadu <rohitkadu3102@gmail.com>
2023-06-18 21:49:48 +0530

    extra or string permutations

---
+++ STL ( Standard Template Lib )/extra.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main()
+{
+    
+    for( int i=1 ; i<=10 ; i++)
+    {
+        printf("\npopcount / Set bits of %d = %d", i, __builtin_popcount(i));
+    }
+
+    long long num = 12837438938432;
+    printf("\npopcount / Set bits of %lld = %d", num, __builtin_popcountll(num));
+
+
+    cout<<"\n\nEnter string : ";
+    string str;
+    getline(cin, str);
+
+    cout<<"\n\tAll permutations of "<<str<<" = ";
+    do{
+        cout<<str<<", ";
+    }while( next_permutation(str.begin(), str.end()));
+
+
+
+
+    return 0;
+}
